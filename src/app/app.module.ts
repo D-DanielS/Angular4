dd04b1c3d61df217e68847a75a84c3e6d248dca3
Juan Lizcano <juanliizp12@gmail.com>
2017-09-25 17:19:59 -0500

    modulo de rutas terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,38 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AgmCoreModule } from '@agm/core';
-import {ResaltarDirective} from './directives/resaltar.directive';
-import {ContarClicksDirective} from './directives/contar-clicks.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
+import { Routes, RouterModule } from '@angular/router';
+import { DetalleComponent } from './detalle/detalle.component';
+import { LugaresComponent } from './lugares/lugares.component';
+import {ContactoComponent} from "./contacto/contacto.component";
 
+const appRoutes: Routes = [
+  {path: '', component: LugaresComponent},
+  {path: 'lugares', component: LugaresComponent},
+  {path: 'detalle/:id', component: DetalleComponent},
+  {path: 'contacto', component: ContactoComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
       ResaltarDirective,
-      ContarClicksDirective
+      ContarClicksDirective,
+      DetalleComponent,
+      LugaresComponent,
+      ContactoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCjdJHaJouM35IYXREAkWce32oL7NcfgEw'
-    })
+    }),
+    BrowserAnimationsModule,
+      RouterModule.forRoot(appRoutes)
 
 
 
---
+++ src/app/contacto/contacto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-contacto',
+  templateUrl: './contacto.component.html'
+})
+export class ContactoComponent {
+    nombre = 'Rous Accesorios';
+    description = 'Descripción de este negocio. Más adelante dentremos mas informacion';
+
+}
