8ce8722368c7214c553f464ebdadb7c679b10542
Juan Lizcano <juanliizp12@gmail.com>
2017-10-02 16:43:57 -0500

    Formateando respuestas del servidor con el operador map()

--- src/app/services/lugares.service.ts
+++ src/app/services/lugares.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
 import {AngularFireDatabase} from "angularfire2/database";
 import {Headers, Http} from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Injectable()
 
@@ -21,7 +22,12 @@ export class LugaresService{
 
     public getLugares(){
         //return this.afDB.list('/lugares')
-        return this.http.get(this.API_ENDPOINT + '/lugares.json');
+        return this.http.get(this.API_ENDPOINT + '/.json')
+            .map((resultado) =>{
+                const data = resultado.json().lugares;
+                return data;
+
+            })
 
     }
     public buscarLugar(id){
